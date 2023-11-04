6f524c502b88766564cf814897ff59a17698b97e
Gabriel <gabrielmancilladucuara@gmail.com>
2023-11-03 20:33:08 -0600

    subiendo datos

---
+++ 08-JourNall_App/src/store/auth/authSlice.js
@@ -0,0 +1,30 @@
+//storage relacionado a la authenticacion
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        status: 'checking',
+        uid: null, 
+        email: null,
+        displayName: null,
+        photoURL: null,
+        errorMessage: null,
+    },
+    reducers: {
+        login:( state , action )=> {
+
+        },
+        logout:( state , payload )=> {
+
+        },
+        //nos sirve para saber si esta authenticado o no
+        checkingCredencials: (state) => {
+
+        }
+    }
+})
+
+//Acciones especificas
+// Action creators are generated for each case reducer function
+export const { login, logout, checkingCredencials} = authSlice.actions
